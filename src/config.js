f50f3aa6daf83a93d59d0acec090988eed6f115a
Dima <fox6383@gmail.com>
2018-09-14 17:00:26 +0300

    111

--- src/config.js
+++ src/config.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase'
 
 export const appName = 'mydiaryv1-bfeac'
+
+
 export const firebaseConfig = {
-    apiKey: "AIzaSyBEeuvoadz4svfxylIIZT4-lfbHA8DMgvk",
-    authDomain: `${appName}.firebaseapp.com`,
-    databaseURL: `https://${appName}.firebaseio.com`,
-    projectId: appName,
-    storageBucket: `${appName}.appspot.com`,
-    messagingSenderId: "877512590866"
+
 }
+
+
+
 firebase.initializeApp(firebaseConfig);
