50a1364e6330fb4d2c56106c7e0b73505b45a626
Dima <fox6383@gmail.com>
2018-09-27 11:19:07 +0300

    Add function image load to firebase store

--- src/components/Layouts/Diary/Note.js
+++ src/components/Layouts/Diary/Note.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react'
+import React, { Component, Fragment, Children } from 'react'
 import {
     withStyles, 
     Button,
@@ -22,6 +22,9 @@ import {
 import {
     userSelector
 } from '../../../ducks/auth'
+
+import firebase from 'firebase'
+
 
 const styles = theme => ({
     paper:{
@@ -102,6 +105,12 @@ class Note extends Component {
                     <Editor
                         editorState={editorState}
                         onEditorStateChange = {this.onEditorStateChange}
+                        toolbar={{
+                            image:{
+                                uploadCallback: uploadImageCallBack,
+                                previewImage:true
+                            }
+                        }}
                     />
                 </Paper>
             </Fragment>
@@ -109,6 +118,61 @@ class Note extends Component {
         
     }
 }
+
+
+function uploadImageCallBack(file) {
+    const storageRef = firebase.storage().ref();
+    return new Promise(
+      (resolve, reject) => {
+        var metadata = {
+            contentType: 'image/jpeg'
+          };
+          
+        var uploadTask = storageRef.child('images/' + file.name).put(file, metadata);
+
+        uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
+        function(snapshot) {
+          // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+          var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log('Upload is ' + progress + '% done');
+          switch (snapshot.state) {
+            case firebase.storage.TaskState.PAUSED: // or 'paused'
+              console.log('Upload is paused');
+              break;
+            case firebase.storage.TaskState.RUNNING: // or 'running'
+              console.log('Upload is running');
+              break;
+          }
+        }, function(error) {
+      
+        // A full list of error codes is available at
+        // https://firebase.google.com/docs/storage/web/handle-errors
+        switch (error.code) {
+          case 'storage/unauthorized':
+          console.log('User doesnt have permission to access the object')
+            // User doesn't have permission to access the object
+            break;
+      
+          case 'storage/canceled':
+            // User canceled the upload
+            break;
+      
+              
+          case 'storage/unknown':
+          console.log('Unknown error occurred, inspect error.serverResponse')
+            // Unknown error occurred, inspect error.serverResponse
+            break;
+        }
+      }, function() {
+        // Upload completed successfully, now we can get the download URL
+        uploadTask.snapshot.ref.getDownloadURL().then(function(downloadURL) {
+          console.log('File available at', downloadURL);
+          resolve({data: {link:downloadURL}})
+        });
+      });
+      }
+    );
+  }
 
 export default withStyles(styles)(connect(
     (state, ownProps) => ({
